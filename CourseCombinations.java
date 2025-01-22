b8d9a89583799c2d0451d84d924db01209273fda
CeasarMusk <ceasarmusk@gmail.com>
2025-01-22 10:32:28 -0500

    HashMap stores info as <String, List<String>>`

--- CourseCombinations.java
+++ CourseCombinations.java
@@ -1,9 +1,13 @@
-import java.io.File
-import java.io.FileNotFoundException
-import java.util.Scanner
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 
-public class popularCourse
+public class CourseCombinations
 {
 	public static void main(String[] args)
 	{
@@ -11,23 +15,42 @@ public class popularCourse
 		{
 			File data = new File("data.txt");
 			Scanner scan = new Scanner(data);
-			/* Example Data.txt
-			 * 5
-			 * Ceasar MATH SCI
-			 * RYAN FUCK ME
-			 *At least 1-20
-			 */
-			int n = Interger.parseInt(scan.nextLine());
+			Map<String, List<String>> studentCourses=new HashMap<>();
+			
+			
+			int n = Integer.parseInt(scan.nextLine());
+			
 			for(int i =0; i< n;i++)
 			{
-				String[] parts = scan.nextLine().split(" ");
+				if(scan.hasNextLine())
+				{
+					String[] parts=scan.nextLine().split(" ");
+			
+					List<String> courses = new ArrayList<>();
+				
+					for(int j = 1; j < parts.length; j++)
+						{	
+							courses.add(parts[j]);
+						}
+
+					studentCourses.put(parts[0],courses);	
+				}
 			}
+
+			scan.close();
+			  for(Map.Entry<String, List<String>> entry : studentCourses.entrySet()){
+                        System.out.println(entry.getKey()+": "+entry.getValue());
+                }
 
 		} catch (FileNotFoundException e)
 		{
 			System.out.println("Something fucked up");
 			e.printStackTrace();
 		}
+	
+	
+	
+
 	}
 }
 
